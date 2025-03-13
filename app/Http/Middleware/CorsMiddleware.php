bbc6667545633513d8ab901446cd6cd0dc7add58
medharik <med.harik@gmail.com>
2025-03-13 01:05:11 +0000

    cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,8 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next): Response
     {
 
-        return $next($request)->header('Access-Control-Allow-Origin', '*') // Allow all origins (*), or specify one
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
